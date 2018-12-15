cb29caa8d6ae49f3934d13821e8fd31e21f30786
Aggie Koko <agata.kosior57@gmail.com>
2018-12-15 13:24:59 +0100

    Add Albums and Artist array to RootQuery, connect albums and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,14 @@ const _ = require('lodash');
 const albums = require('./data').albums;
 const artists = require('./data').artists;
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList
+} = graphql;
 
 const AlbumType = new GraphQLObjectType({
   name: 'Album',
@@ -13,22 +20,28 @@ const AlbumType = new GraphQLObjectType({
     genre: { type: GraphQLString },
     year: { type: GraphQLInt },
     artist: {
-        type: ArtistType,
-        resolve(parent, args) {
-          return _.find(artists, { id: parent.artistId });
-        }
+      type: ArtistType,
+      resolve(parent, args) {
+        return _.find(artists, { id: parent.artistId });
       }
+    }
   })
 });
 
 const ArtistType = new GraphQLObjectType({
-    name: 'Artist',
-    fields: () => ({
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      origin: { type: GraphQLString }
-    })
-  });
+  name: 'Artist',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    origin: { type: GraphQLString },
+    albums: {
+      type: new GraphQLList(AlbumType),
+      resolve(parent, args) {
+        return _.filter(albums, { artistId: parent.id });
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -45,6 +58,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(artists, { id: args.id });
+      }
+    },
+    albums: {
+      type: new GraphQLList(AlbumType),
+      resolve(parent, args) {
+        return albums;
+      }
+    },
+    artists: {
+      type: new GraphQLList(ArtistType),
+      resolve(parent, args) {
+        return artists;
       }
     }
   }
